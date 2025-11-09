1d9167631acc3167d722156afca282381d803233
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-09 21:08:07 +0545

    init of windowGameOver

--- main.c
+++ main.c
@@ -30,7 +30,10 @@ static void activate(GtkApplication *app,gpointer user_data) {
     //Activates the Program
     mainWindow();
 
-
+    //Init of windowGameOver
+    windowGameOver = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(windowGameOver),"GAME OVER!");
+    gtk_window_set_default_size(GTK_WINDOW(windowGameOver),200,200);
 
 
 }
